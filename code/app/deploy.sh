6e2b65c328c85b2a8508e14bbf70c4547693e88b
Egle Sigler <ushnishtha@hotmail.com>
2015-10-01 23:03:20 -0500

    Updating slides and changing links to repo
    Changes all the repo links to my repo for now.

--- code/app/deploy.sh
+++ code/app/deploy.sh
@@ -7,7 +7,7 @@ echo "source /root/app_env.rc" >> /root/.profile
 echo "source /root/os_env.rc" >> /root/.profile
 source /root/.profile
 
-git clone https://github.com/everett-toews/app-on-openstack.git /root/app-on-openstack
+git clone https://github.com/eglute/app-on-openstack.git /root/app-on-openstack
 pip install virtualenv
 virtualenv /root/app-venv
 source /root/app-venv/bin/activate
